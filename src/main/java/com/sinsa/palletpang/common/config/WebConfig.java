2d40a6de0e640dbb35a77bbb7a81c41a28b3de97
deneb1593 <deneb1593@gmail.com>
2022-07-27 17:32:42 +0900

    logback 설정

---
+++ src/main/java/com/sinsa/palletpang/common/config/WebConfig.java
@@ -0,0 +1,33 @@
+package com.sinsa.palletpang.common.config;
+
+import com.sinsa.palletpang.common.security.WebSecurityConfig;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] {RootConfig.class, WebSecurityConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {ServletConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setForceEncoding(true);
+        encodingFilter.setEncoding("UTF-8");
+        return new Filter[] { encodingFilter };
+    }
+}
---
+++ src/main/java/com/sinsa/palletpang/common/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.sinsa.palletpang.common.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
